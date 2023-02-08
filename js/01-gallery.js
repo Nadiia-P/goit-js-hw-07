4d9a6a4bf67cbc0cf04127c91c2e6d006abcd146
Nadiia P <nadiia.parakhina@gmail.com>
2023-02-08 16:19:32 +0200

    rendered previews

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryRender = galleryItems
+  .map(
+    (galleryItem) =>
+      `<div class="gallery__item">
+    <a
+      href="${galleryItem.original}"
+      class="gallery__link">
+      <img
+        class="gallery__preview"
+        src="${galleryItem.preview}"
+        alt="${galleryItem.description}" />
+    </a>
+  </div>`
+  )
+  .join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryRender);
