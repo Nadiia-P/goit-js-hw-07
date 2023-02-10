e101de563368224b048d2fec5855fbc0f6fdb0e4
Nadiia P <nadiia.parakhina@gmail.com>
2023-02-10 12:59:14 +0200

    simple light bos is done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryRender = galleryItems
+  .map(
+    (galleryItem) =>
+      `<a href="${galleryItem.original}"
+      class="gallery__link">
+      <img
+        class="gallery__preview"
+        src="${galleryItem.preview}"
+        alt="${galleryItem.description}" />
+    </a>`
+  )
+  .join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryRender);
+
+galleryContainer.addEventListener('click', onPreviewClick);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  showCounter: false,
+});
+
+function onPreviewClick(e) {
+  if (e.target.nodeName !== 'IMG') return;
+  e.preventDefault();
+}
